aafe830b97cfc85eb932e9bd9484d490cf9f5661
root <root@vmi2974289.contaboserver.net>
2026-01-02 02:44:35 +0100

    Fix: Resolve continuous app crashes by adding missing colors.xml and updating MainActivity splash screen logic

--- android/app/src/main/java/com/kantinho/delicia/MainActivity.java
+++ android/app/src/main/java/com/kantinho/delicia/MainActivity.java
@@ -1,5 +1,12 @@
 package com.kantinho.delicia;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        androidx.core.splashscreen.SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+    }
+}
